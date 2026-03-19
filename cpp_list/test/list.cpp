d8d72aa18af7d4abff706a8845945a028621343e
NikiHuber <s2510828009@fhooe.at>
2026-03-19 10:49:44 +0100

    integrate UnitTests

---
+++ cpp_list/test/list.cpp
@@ -0,0 +1,13 @@
+#include "list.hpp"
+#include <gtest/gtest.h>
+
+TEST(LIST, insert_element){
+    List list;
+
+    EXPECT_EQ(list.length(), 0);
+    list.push_back(123);
+    EXPECT_EQ(list.length(), 1);
+    int value;
+    ASSERT_TRUE(list.get(0, value));
+    ASSERT_EQ(value, 123);
+}
