b92c237601f119fd2c5b7172010133df1b87c1d3
Peter Grininger <s2510828006@fhooe.at>
2026-03-19 10:34:12 +0100

    Tests

---
+++ cpp_list/test/list.cpp
@@ -0,0 +1,22 @@
+# include "list.hpp"
+#include <gtest/gtest.h>
+
+
+
+TEST(List, get_on_emty){
+    List list;
+    int value;
+    ASSERT_FALSE(list.get(0,value));
+}
+
+
+TEST(list, insert_element){
+    List list;
+    EXPECT_EQ(list.length(), 0); // Expect erwarten und wenns nicht funktioniert mache ich weiter
+    list.push_back(123);
+    EXPECT_EQ(list.length(), 1);
+    int value;
+    ASSERT_TRUE(list.get(0,value)) ;
+    ASSERT_EQ(value, 123); // Assert erwarten und wenns nicht funktioniert bricht er ab
+}
+
