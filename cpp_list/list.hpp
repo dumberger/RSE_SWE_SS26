2a17c2cf94eb447e495ef7693a62ef414d5844b1
schaetztho <Thomas.Schaetz@students.fh-wels.at>
2026-03-19 08:57:55 +0100

    test

--- cpp_list/list.hpp
+++ cpp_list/list.hpp
@@ -1,21 +1,23 @@
 #pragma once
 
-class List 
-{
+class List {
 private:
-    struct ListNode {
-        int value;
-        struct ListNode* next;
-    };
-    
-    ListNode* first;
-public:
-    List();
-    ~List();
+  struct ListNode {
+    int value;
+    struct ListNode *next;
+  };
 
-    bool get(unsigned int index, int& val);
-    void push_back(int value);
-    void remove(unsigned int index);
-    void print();
-    unsigned int length();
+  ListNode *first;
+
+public:
+  List();
+  ~List();
+
+  bool get(unsigned int index, int &val);
+  void push_back(int value);
+  void remove(unsigned int index);
+  void print();
+  unsigned int length();
+
+  // git test
 };
