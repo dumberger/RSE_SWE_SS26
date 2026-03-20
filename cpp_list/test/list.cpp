dc0f49738b8f541e04f1deda5dbe3c16cabaa414
Michael Tichelmann <61211255+madtic@users.noreply.github.com>
2026-03-20 13:10:46 +0100

    -adding tests get node

--- cpp_list/test/list.cpp
+++ cpp_list/test/list.cpp
@@ -29,11 +29,35 @@ TEST(LIST, insert_element) {
 TEST(LIST, get_value) {
     List list;
     int value;
+    //get from no elements
     ASSERT_FALSE(list.get(0, value));
+    //get from list
     int insertion_value = 9999;
     list.push_back(insertion_value);
     ASSERT_TRUE(list.get(0, value));
     ASSERT_EQ(value, insertion_value);
+
+    //add more
+    list.push_back(100);
+    list.push_back(100);
+    ASSERT_TRUE(list.get(0, value));
+    ASSERT_EQ(value, insertion_value);
+}
+
+TEST(LIST, get_node) {
+    List list;
+    //empty list
+    NodeList* node;
+    ASSERT_FALSE(list.getNode(100, node));
+
+    //list element
+    list.push_back(100);
+    list.push_back(200);
+    list.push_back(300);
+    ASSERT_TRUE(list.getNode(1, node));
+    ASSERT_EQ(node.Value, 200);
+    ASSERT_EQ(node.prev, list.first);
+    ASSERT_EQ(node.next, list.last);
 }
 
 //Testing insert and remove at zero
