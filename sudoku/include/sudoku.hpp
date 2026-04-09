f0704051d1c8b7125b378b9dc0b1f7f73f28c0b6
schaetztho <Thomas.Schaetz@students.fh-wels.at>
2026-04-09 13:40:04 +0200

    Sudoku

---
+++ sudoku/include/sudoku.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <string>
+
+// defines a NxN Sudoku
+template <std::size_t N> class Sudoku {
+private:
+  std::array<std::array<unsigned int, N>, N> field; // the Sudoku field
+  std::string SYMBOLS =
+      "_123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // symbols for output
+
+public:
+  Sudoku() {
+    for (auto &row : field) {
+      for (auto &cell : row) {
+        cell = 0;
+      }
+    }
+  }
+
+  // ~Sudoku() = default;
+
+  bool set(std::size_t row, std::size_t col, char value);
+  char get(std::size_t row, std::size_t col);
+};
---
+++ sudoku/src/main.cpp
@@ -0,0 +1,11 @@
+#include "sudoku.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  Sudoku<9> test;
+
+  return 0;
+};
