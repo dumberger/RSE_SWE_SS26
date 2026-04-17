a04f018b7f9735639445151e042c5278bcc50ebf
Erik Stelzer <erik.stelzer@students.fh-wels.at>
2026-04-17 06:51:22 +0200

    sudoku.hpp lecture

--- sudoku/include/sudoku.hpp
+++ sudoku/include/sudoku.hpp
@@ -21,10 +21,60 @@ class Sudoku {
 
         //~Sudoku() = default;
 
-        bool set(std::size_t row, std::size_t col, char value);
-        char get(std::size_t row, std::size_t col);
+        bool set(std::size_t row, std::size_t col, char value)
+        {
+            /*
+            auto i = std::find(SYMBOLS.begin(), SYMBOLS.end(), value);
+            if (i == SYMBOLS.end())
+            {
+                 return false;
+            }
+            
+            int index = i - SYMBOLS.begin();
+            */
+
+            auto index = SYMBOLS.find(value);
+
+            if(index == std::string::npos)
+            {
+                return false;
+            }
+            if(index > N)
+            {
+                return false;
+            }
+            if(!check_rules(row, col, index))
+            {
+                return false;
+            }
+
+            field[row][col] = index;
+            rows[row].set(index-1);
+            cols[col].set(index-1);
+            blks[calculate_block(row, col)].set(index-1);
+            return true;
+        }
+
+        char get(std::size_t row, std::size_t col) { return SYMBOLS[field[row][col]]};
 
     private:
         std::array<std::array<unsigned int, N>, N> field;
+        std::array<std::bitset<N>, N> rows;
+        std::array<std::bitset<N>, N> cols;
+        std::array<std::bitset<N>, N> blks;
         std::string SYMBOLS = "_123456789ABCDEFGHIJKLMNOQRSTUVWXYZ";
+
+        int calculate_block(std::size_t row, std::size_t col)
+        {
+            std::size_t block_size = sqrt(N);
+            // integer divisions will automatically be rounded down
+            return (row / block_size) * block_size+ (col / block_size);
+        }
+
+        bool check_rules(std::size_t row, std::size_t col, int index)
+        {
+            std::bitset<N> result = rows[row] | cols[col] | blks[calculate_block(row, col)];
+            std::cout << result.to_string() << std::endl;
+            return !result[index];
+        }
 };
