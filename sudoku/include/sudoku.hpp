6430ade4db6cf6887abab67c8e3e6d0212851701
Peter Grininger <s2510828006@fhooe.at>
2026-04-18 11:17:59 +0200

    Notes from class (sudoku updates)

--- sudoku/include/sudoku.hpp
+++ sudoku/include/sudoku.hpp
@@ -1,15 +1,69 @@
 #pragma once
 
+#include <algorithm>
+#include <bitset>
+#include <cmath>
 #include <cstddef>
 #include <array>
+#include <functional>
+#include <istream>
+#include <ostream>
 #include <string>
+#include <iostream>
 
 // defines a NxN Sudoku
+template<std::size_t N> class Sudoku;
+template<std::size_t N>
+std:: ostream& operator <<(std::ostream& stream, Sudoku<N>& sudoku){
+    for (auto& row : sudoku.field) {
+            for (auto& cell : row) {
+                stream <<sudoku.SYMBOLS[cell]<<" ";
+            }
+            stream<<"\n";
+        }
+    return stream;
+}
+
+template <std::size_t N>
+std::istream& operator >>(std::istream& stream, Sudoku<N>& sudoku){
+    for (int row = 0;  row < N ; ++row) {
+    
+        for (int col = 0;  col < N ; ++col) {
+        
+            char value;
+            stream >> value;
+            if(!sudoku.set(row,col, value)){
+                stream.setstate(std::ios::failbit);
+                return stream;
+            }
+        }
+    }
+    return stream;
+
+
+}
+
+
 template<std::size_t N>
 class Sudoku {
+
+    //get the row and col of the next placeholder in the sudoku
+    //get (N,N) when the sudoku is fully solved
+    std::pair<std:: size_t,std::size_t>next(){
+        std::pair<int,int>pair;
+    }
 private:
-    std::array<std::array<unsigned int, N>, N> field;
+    
+    //placeholder symbol is always the first symbol
     std::string SYMBOLS="_123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    std::array<std::array<unsigned int, N>, N> field;
+    std::array<std::bitset<N>, N> rows;
+    std::array<std::bitset<N>, N> cols;
+    std::array<std::bitset<N>, N> blks;
+
+    template<std::size_t M>
+    friend std:: ostream& operator <<(std::ostream& stream, Sudoku<M>&);
 
 public:
     Sudoku() {
@@ -22,19 +76,109 @@ public:
 
     //~Sudoku() = default;
 
-    bool set(std::size_t row, std::size_t col, char value);
-    char get(std::size_t row, std::size_t col);
+    
+    char get(std::size_t row, std::size_t col){return SYMBOLS[ field[row][col]];};
+
+    bool set(std::size_t row, std::size_t col, char value)
+    {
+        if(row >= N || col >= N){
+            return false;
+        }
+        // auto i = std::find(SYMBOLS.begin(),SYMBOLS.end(),value);
+        // if(i == SYMBOLS.end()){
+        //     return false;
+        // }
+
+        
+        //int index = SYMBOLS.find(value);
+        //std::size_t index = SYMBOLS.find(value);
+        auto index = SYMBOLS.find(value);
+        if (index == std::string::npos){
+            return false;
+        }
+        if(index > N){
+            return false;
+        }
+
+        // rauslöschen und die alten Werte rausholen
+        remove_previous_element(row, col);
+        if(index==0){
+            field[row][col] =0;
+            return true;
+        }
+
+
+        if(!check_rules(row,col,index)){
+
+            std::size_t old_index = field[row][col];
+            if(old_index ==0){
+                return false;
+            }
+            rows[row].set(index-1);
+            cols[col].set(index-1);
+            blks[calculate_block(row, col)].set(old_index);
+            return false;
+        }
+
+        field[row][col] = index;
+        rows[row].set(index-1);
+        cols[col].set(index-1);
+        blks[calculate_block(row, col)].set(index-1);
+        return true;
+
+    }
+
+
+    private:
+
+        int calculate_block(std::size_t row, std::size_t col){
+            std::size_t block_size = sqrt(N);
+            return floor(row /block_size) * block_size + floor(col / block_size) ; // immer abgerundet bei der Division (0 bis 2 = 0 / 3 bis 5 = 1)
+        }
+
+        bool check_rules (std::size_t row, std::size_t col, int index){
+            // is index in this line
+            // std::bitset<N> row_ = rows[row];
+            // if(row_[index-1]){
+            //     return false;
+            // }
+
+            // std::bitset<N> col_ = cols[col];
+            // if(col_[index-1]){
+            //     return false;
+            // }
+
+            // std::bitset<N> blk_ = blks[calculate_block(row, col)];
+            // if(blk_[index-1]){
+            //     return false;
+            // }
+            // return true;
+
+            // is index in this colomn
+            // is index in this block
+
+            std::bitset<N> result = rows[row] |cols[col]|blks[calculate_block(row, col)];
+            std::cout << result.to_string()<< std::endl;
+            return !result[index-1];
+
+        }
+
+        void remove_previous_element(std:: size_t row, std::size_t col){
+
+            std::size_t index = field[row][col];
+            if(index == 0){
+                return;
+            }
+            rows[row].reset(index-1);
+            cols[col].reset(index-1);
+            blks[calculate_block(row, col)].reset(index-1);
+
+        }
+
+
+
 };
 
 
 
-// Für die Tests (muss noch fertig implementiert werden)
-template<std::size_t N>
-bool Sudoku<N>::set(std::size_t row, std::size_t col, char value) {
-    return false; // Noch leer gelassen
-}
 
-template<std::size_t N>
-char Sudoku<N>::get(std::size_t row, std::size_t col) {
-    return '_'; // Einfach einen Standardwert zurückgeben für das Kompilieren
-}
--- sudoku/src/main.cpp
+++ sudoku/src/main.cpp
@@ -1,10 +1,39 @@
+#include <filesystem>
 #include <iostream>
 #include "../include/sudoku.hpp"
+#include <fstream>
+#include <strstream>
+#include <strstream>
+#include <system_error>
 
 using namespace std;
 
 int main() {
-    Sudoku<9> test;
+    // Sudoku<9> test;
+
+    // test.set(0, 0, '1');
+    // test.set(0, 1, '2');
+    // test.set(0, 0, '2');
+    // test.set(0, 3, '4');
+    // char symbol = test.get(0, 0);
+    // printf("%c\n\n",symbol);
+
+    std::filesystem::path directory(__FILE__);
+    directory = directory.parent_path();
+
+    std::ofstream output(directory.string()+"/template.txt");
+    Sudoku<9> sudoku_template;
+    output << sudoku_template;
+
+    std::ifstream file(directory.string()+"/input.txt");
+    // std::strstream ss;
+    // ss << test << "\n";
+    Sudoku<9> copy;
+    file >> copy;
+    std::cout << copy;
+    if(!file.good()){
+        std::cout<<"error reading file \n";
+    }
 
     return 0;
 }
