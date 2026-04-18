9e06700772740f0f817eaa8531aaef1d42bab745
schaetztho <Thomas.Schaetz@students.fh-wels.at>
2026-04-18 12:56:58 +0200

    list test fixed

--- cpp_list/test/list.cpp
+++ cpp_list/test/list.cpp
@@ -1,49 +1,47 @@
-#include "list.hpp"
+#include "../include/list.hpp"
 #include <gtest/gtest.h>
-#include <iostream>
-#include <sstream>
 
 // get() Tests
 TEST(LIST, get_on_empty) {
-  List list;
+  List<int> list;
   int value;
-  ASSERT_FALSE(list.get(0, &value));
+  ASSERT_FALSE(list.get(0, value));
 }
 
 TEST(LIST, get_single_element) {
-  List list;
+  List<int> list;
   list.push_back(123);
   int value;
-  ASSERT_TRUE(list.get(0, &value));
+  ASSERT_TRUE(list.get(0, value));
   ASSERT_EQ(value, 123);
 }
 
 TEST(LIST, get_multiple_elements) {
-  List list;
+  List<int> list;
   list.push_back(123);
   list.push_back(456);
   int value;
-  ASSERT_TRUE(list.get(0, &value));
+  ASSERT_TRUE(list.get(0, value));
   ASSERT_EQ(value, 123);
-  ASSERT_TRUE(list.get(1, &value));
+  ASSERT_TRUE(list.get(1, value));
   ASSERT_EQ(value, 456);
 }
 
 TEST(LIST, get_out_of_bounds) {
-  List list;
+  List<int> list;
   list.push_back(123);
   int value;
-  ASSERT_FALSE(list.get(1, &value));
+  ASSERT_FALSE(list.get(1, value));
 }
 
 // length() Tests
 TEST(LIST, length_on_empty) {
-  List list;
+  List<int> list;
   ASSERT_EQ(list.length(), 0);
 }
 
 TEST(LIST, length_after_inserts) {
-  List list;
+  List<int> list;
   list.push_back(123);
   list.push_back(456);
   ASSERT_EQ(list.length(), 2);
@@ -51,31 +49,31 @@ TEST(LIST, length_after_inserts) {
 
 // push_back() Tests
 TEST(LIST, insert_element) {
-  List list;
+  List<int> list;
   EXPECT_EQ(list.length(), 0);
   list.push_back(123);
   EXPECT_EQ(list.length(), 1);
   int value;
-  ASSERT_TRUE(list.get(0, &value));
+  ASSERT_TRUE(list.get(0, value));
   ASSERT_EQ(value, 123);
 }
 
 TEST(LIST, insert_multiple_elements) {
-  List list;
+  List<int> list;
   EXPECT_EQ(list.length(), 0);
   list.push_back(123);
   list.push_back(456);
   EXPECT_EQ(list.length(), 2);
   int value;
-  ASSERT_TRUE(list.get(0, &value));
+  ASSERT_TRUE(list.get(0, value));
   ASSERT_EQ(value, 123);
-  ASSERT_TRUE(list.get(1, &value));
+  ASSERT_TRUE(list.get(1, value));
   ASSERT_EQ(value, 456);
 }
 
 // remove() Tests
 TEST(LIST, remove_single_element) {
-  List list;
+  List<int> list;
   list.push_back(123);
   EXPECT_EQ(list.length(), 1);
   list.remove(0);
@@ -83,33 +81,33 @@ TEST(LIST, remove_single_element) {
 }
 
 TEST(LIST, remove_multiple_elements) {
-  List list;
+  List<int> list;
   list.push_back(123);
   list.push_back(456);
   EXPECT_EQ(list.length(), 2);
   list.remove(0);
   EXPECT_EQ(list.length(), 1);
   int value;
-  ASSERT_TRUE(list.get(0, &value));
+  ASSERT_TRUE(list.get(0, value));
   ASSERT_EQ(value, 456);
   list.remove(0);
   EXPECT_EQ(list.length(), 0);
 }
 
 TEST(LIST, remove_out_of_bounds) {
-  List list;
+  List<int> list;
   list.push_back(123);
   EXPECT_EQ(list.length(), 1);
   list.remove(1);
   EXPECT_EQ(list.length(), 1);
   int value;
-  ASSERT_TRUE(list.get(0, &value));
+  ASSERT_TRUE(list.get(0, value));
   ASSERT_EQ(value, 123);
   ASSERT_TRUE(list.length() == 1);
 }
 
 TEST(LIST, remove_from_empty) {
-  List list;
+  List<int> list;
   EXPECT_EQ(list.length(), 0);
   list.remove(0);
   ASSERT_EQ(list.length(), 0);
@@ -117,7 +115,7 @@ TEST(LIST, remove_from_empty) {
 
 // print() Tests
 TEST(LIST, print_empty) {
-  List list;
+  List<int> list;
   testing::internal::CaptureStdout();
   list.print();
   std::string output = testing::internal::GetCapturedStdout();
@@ -125,7 +123,7 @@ TEST(LIST, print_empty) {
 }
 
 TEST(LIST, print_single_element) {
-  List list;
+  List<int> list;
   list.push_back(123);
   testing::internal::CaptureStdout();
   list.print();
@@ -135,7 +133,7 @@ TEST(LIST, print_single_element) {
 
 // printReverse() Tests
 TEST(LIST, print_reverse_empty) {
-  List list;
+  List<int> list;
   testing::internal::CaptureStdout();
   list.printReverse();
   std::string output = testing::internal::GetCapturedStdout();
@@ -143,7 +141,7 @@ TEST(LIST, print_reverse_empty) {
 }
 
 TEST(LIST, print_reverse_single_element) {
-  List list;
+  List<int> list;
   list.push_back(123);
   testing::internal::CaptureStdout();
   list.printReverse();
