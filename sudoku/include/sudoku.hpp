3bb36f6bac2ef00540c38721a0df415d427f8969
Erik Stelzer <erik.stelzer@students.fh-wels.at>
2026-04-21 10:15:06 +0200

    working version of sudoku (lecture)

--- sudoku/include/sudoku.hpp
+++ sudoku/include/sudoku.hpp
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <array>
 #include <string>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <ios>
+#include <ostream>
+#include <iostream>
+#include <utility>
 
 template<std::size_t N>
 class Sudoku {
@@ -21,6 +29,11 @@ class Sudoku {
 
         //~Sudoku() = default;
 
+        char get(std::size_t row, std::size_t col) 
+        { 
+            return SYMBOLS[field[row][col]]; 
+        }
+
         bool set(std::size_t row, std::size_t col, char value)
         {
             /*
@@ -32,6 +45,11 @@ class Sudoku {
             
             int index = i - SYMBOLS.begin();
             */
+            
+            if (row >= N || col >= N) 
+            {
+                return false;
+            }
 
             auto index = SYMBOLS.find(value);
 
@@ -39,12 +57,30 @@ class Sudoku {
             {
                 return false;
             }
+
             if(index > N)
             {
                 return false;
             }
+
+            remove_previous_element(row, col);
+
+            if(index == 0)
+            {
+                field[row][col] = 0;
+                return true;
+            }
+
             if(!check_rules(row, col, index))
             {
+                std::size_t old_index = field[row][col];
+                if(old_index == 0)
+                {
+                    return false;
+                }
+                rows[row].set(old_index-1);
+                cols[col].set(old_index-1);
+                blks[calculate_block(row, col)].set(old_index-1);
                 return false;
             }
 
@@ -55,7 +91,9 @@ class Sudoku {
             return true;
         }
 
-        char get(std::size_t row, std::size_t col) { return SYMBOLS[field[row][col]]};
+        // get the row and col of the next placeholder in the sodoku
+        // get (N,N) when the sudoku is fully solved
+        std::pair<std::size_t, std::size_t> next();
 
     private:
         std::array<std::array<unsigned int, N>, N> field;
@@ -64,6 +102,10 @@ class Sudoku {
         std::array<std::bitset<N>, N> blks;
         std::string SYMBOLS = "_123456789ABCDEFGHIJKLMNOQRSTUVWXYZ";
 
+        template<std::size_t M>
+        friend std::ostream& operator<<(std::ostream&, Sudoku<M>&);
+
+    private:
         int calculate_block(std::size_t row, std::size_t col)
         {
             std::size_t block_size = sqrt(N);
@@ -75,6 +117,45 @@ class Sudoku {
         {
             std::bitset<N> result = rows[row] | cols[col] | blks[calculate_block(row, col)];
             std::cout << result.to_string() << std::endl;
-            return !result[index];
+            return !result[index-1];
+        }
+
+        void remove_previous_element(std::size_t row, std::size_t col) 
+        {
+            std::size_t index = field[row][col];
+            if (index == 0) 
+            {
+                return;
+            }
+            rows[row].reset(index - 1);
+            cols[col].reset(index - 1);
+            blks[calculate_block(row, col)].reset(index -1);
         }
 };
+
+template<std::size_t N>
+std::ostream& operator<<(std::ostream& stream, Sudoku<N>& sudoku) {
+    for (auto& row : sudoku.field) {
+            for (auto& cell : row) {
+                stream << sudoku.SYMBOLS[cell] << " ";
+            }
+            stream << "\n";
+        }
+    return stream;
+}
+
+template<std::size_t N>
+std::istream& operator>>(std::istream& stream, Sudoku<N>& sudoku) {
+    for (int row = 0; row < N; ++row) {
+        for (int col = 0; col < N; ++col) {
+            char value;
+            stream >> value;
+            if(!sudoku.set(row, col, value))
+            {
+                stream.setstate(std::ios::failbit);
+                return stream;
+            }
+        }
+    }
+    return stream;
+}
