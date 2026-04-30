f10266ec002df7f18df041849b970c2ade7fc70a
madtic <61211255+madtic@users.noreply.github.com>
2026-04-30 15:06:31 +0000

    -methods of sudoku defined

--- sudoku/src/sudoku_wrapper.cpp
+++ sudoku/src/sudoku_wrapper.cpp
@@ -4,5 +4,8 @@
 PYBIND11_MODULE(sudoku_py, m) {
     m.doc() = "pybind11 python -> sudoku c++";
     pybind11::class_<Sudoku<9>>(m, "sudoku")
-        .def(pybind11::init<>());
+        .def(pybind11::init<>())
+        .def("get", &Sudoku<9>::get)
+        .def("set", &Sudoku<9>::set)
+        .def("next", &Sudoku<9>::next);
 }
