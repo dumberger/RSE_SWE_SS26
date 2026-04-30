37a756f311f91deaf531553ceee305463ce3f1bf
madtic <61211255+madtic@users.noreply.github.com>
2026-04-30 15:05:03 +0000

    -wrap sudoku class

--- sudoku/src/sudoku_wrapper.cpp
+++ sudoku/src/sudoku_wrapper.cpp
@@ -1,7 +1,8 @@
-#include <pybind11/detail/common.h>
 #include <pybind11/pybind11.h>
-#include "sudoku.hpp"
+#include "../include/sudoku.hpp"
 
 PYBIND11_MODULE(sudoku_py, m) {
-
+    m.doc() = "pybind11 python -> sudoku c++";
+    pybind11::class_<Sudoku<9>>(m, "sudoku")
+        .def(pybind11::init<>());
 }
