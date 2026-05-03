e73d75a305c3a40ed4fe122d0cc023ef0d3fef29
KissEmil-RSE <s2510828010@fhooe.at>
2026-05-03 11:18:47 +0200

    Pybind-Lecture

---
+++ pybind/adder.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+
+int increment_loop(int start, int end) 
+{
+    int n = start;
+
+    for (int i = start; i < end; i++)
+    {
+        n = n + 1;
+    }
+
+    return n;
+}
+
+PYBIND11_MODULE(adder, m) 
+{
+    m.doc() = "fancy adder module";
+
+    m.def("inc", &increment_loop, "add values in a loop");
+}
