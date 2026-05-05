dc5656eb5c7ad0cfe8544d6599507f92901d32d8
Hala222 <s2510828018@fhooe.at>
2026-05-05 20:49:50 +0000

    UI Python

--- sudoku/python/sudoku.py
+++ sudoku/python/sudoku.py
@@ -88,6 +88,7 @@ class SudokuUI:
                 entry = self.entries[r][c]
 
                 #TODO: read a value from the sudoku class and store in value variable
+                value = self.sudoku.get(r, c)
 
                 entry.config(bg="white")
                 entry.delete(0, tk.END)
@@ -97,6 +98,7 @@ class SudokuUI:
     def set_cell(self, row, col, value):
 
         #TODO: interact with C++ Sudoku here and set valid variable
+        valid = self.sudoku.set(row, col, value)
 
         if not valid:
             entry = self.entries[row][col]
@@ -107,6 +109,7 @@ class SudokuUI:
         print("solving...")
 
         #TODO: call solver to solve the sudoku
+        self.sudoku.solve()
 
         self.sync_with_sudoku_class()
 
@@ -114,6 +117,7 @@ class SudokuUI:
         print("generating a new Sudoku")
 
         #TODO: call sudoku generator
+        self.sudoku.generate()
 
         self.sync_with_sudoku_class()
 
