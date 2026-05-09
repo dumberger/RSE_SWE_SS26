c6d5d5ee378bf54d7975c0a6745040f7499776e2
Peter Grininger <s2510828006@fhooe.at>
2026-05-09 09:24:29 +0200

    merge

--- pybind/adder.cpp
+++ pybind/adder.cpp
@@ -1,17 +1,15 @@
-#include <pybind11/detail/common.h>
-# include <pybind11/pybind11.h>
+#include <pybind11/pybind11.h>
 
-int increment_loop(int start, int end){
+int increment_loop(int start, int end) {
     int n = start;
-    for (int i =start; i< end; i++) {
-        n = n+1;
+    for (int i = start; i < end; i++) {
+        n = n + 1;
     }
     return n;
 }
 
-
 PYBIND11_MODULE(adder, m) {
-    m.doc() = "fancy adder module"; // Beachte: .doc() ist oft ein Attribut
-    m.def("inc", &increment_loop, "add values in a loop");
+    m.doc() = "fancy adder module";
 
+    m.def("inc", &increment_loop, "add values in a loop");
 }
