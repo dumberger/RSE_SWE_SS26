6cd8b737ce78e61af7492d3d97f680209b9f49c8
adrianbier <ichadi2611@gmail.com>
2026-05-14 14:57:04 +0200

    HÜ 6 und 7

--- sudoku/python/python_wrapper.cpp
+++ sudoku/python/python_wrapper.cpp
@@ -1,5 +1,7 @@
 #include <pybind11/pybind11.h>
 #include "sudoku.hpp"
+#include "solver.hpp"
+#include "sudoku_generator.hpp"
 
 PYBIND11_MODULE(sudoku_py, m){
     m.doc() = "Sudoku wrapper for python";
@@ -9,4 +11,18 @@ PYBIND11_MODULE(sudoku_py, m){
         .def("get", &Sudoku<9>::get)
         .def("set", &Sudoku<9>::set)
         .def("next", &Sudoku<9>::next);
+
+    
+    pybind11::class_<Solver>(m, "solver")
+        .def(pybind11::init())
+        .def("solve", &Solver::solve)
+        // Lambda-Funktion, um den Dateipfad für base_directory zu vereinfachen
+        .def("loadSudoku", [](Solver& s, const Sudoku<9>& ref) {
+            return s.loadSudoku(ref, "results");
+        });
+
+    
+    pybind11::class_<SudokuGenerator>(m, "generator")
+        .def(pybind11::init())
+        .def("generate", &SudokuGenerator::generate);
 }
